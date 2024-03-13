3902dedf1215fcc7af175a8df295f6d69147a09a
Редкошеев Александр <liders05@mail.ru>
2024-03-13 13:12:23 +0500

    change the name of the service

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,7 +5,7 @@ namespace  PrestaShop\Module\Dottgnotices\Controller;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use PrestaShop\Module\Dottgnotices\Service\DotBank;
+use PrestaShop\Module\Dottgnotices\Service\AuthorizationBank;
 
 class MainController extends FrameworkBundleAdminController 
 {
