67ae8202c9ebd226be4b8656ab233d02804b9eaa
Редкошеев Александр <liders05@mail.ru>
2024-11-15 19:01:19 +0500

    add webhook controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,32 +15,5 @@ class MainController extends FrameworkBundleAdminController
         $authUrl = $bank->authorize();
  
         return $this->redirect($authUrl);      
-    }
-
-    public function createWebHook(Request $request): Response
-    {
-        $bank = $this->get('prestashop.module.dottgnotices.service.authorization_bank');
-        
-        $webhook = '';
-        if ($bank->checkAuthorization()) {
-            $authorization = 'Вы авторизованы';
-
-            if (!$bank->checkWebHooks()) {
-                $bank->deleteWebHook();
-                $bank->createWebHook();
-                $webhook = 'Вебхук обновлен';
-            } else {
-                $webhook = 'Вебхук не нуждается в обновлении.';
-            } 
-        } else {
-            $authorization = 'Требуется авторизация!!!';
-        }
-
-        return $this->render('@Modules/dottgnotices/views/templates/admin/auth.html.twig', [
-            'autorization' => $success_authorization,
-            'webhook' => $webhook,
-        ]);
-        
-    }
-    
+    }    
 }
